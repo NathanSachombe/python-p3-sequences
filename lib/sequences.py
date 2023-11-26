bec98dd944124a1a0b8dc7da51dc679b331f240a
NathanSachombe <nathanguyo6@gmail.com>
2023-11-26 12:16:01 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a = 0
+    b = 1
+    for i in range(0,length):
+        print(a,end = " ")
+        c=a+b
+        a=b
+        b=c
+
+print_fibonacci(0)
+print_fibonacci(1)
+print_fibonacci(2)
+print_fibonacci(10)
+
+
